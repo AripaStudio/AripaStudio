cb2d79f8e370da60db76bd995b11e1fe1868b330
khashayar <aripastudio44@gmail.com>
2025-06-01 10:48:35 -0700

    Edit ChangeColor.js

--- js/change_color_hmPage.js
+++ js/change_color_hmPage.js
@@ -1,5 +1,13 @@
+//index.html:
 const lbl_welcome = document.getElementById("welcome_homePage");
 const lbl_info_head = document.getElementById("info_studio");
+//aboutme.html:
+const  lbl_Head_games = document.getElementById("head_games");
+const lbl_info_games = document.getElementById("info_games");
+//games-software.html:
+const lbl_Head_aboutme = document.getElementById("head_aboutme");
+const lbl_info_aboutme = document.getElementsByClassName("info_aboutme");
+
 
 let neonColors = ["#0ff", "#ff0", "#f0f", "#0f0", "#0ff", "#ff0066", "#00ffff", "#99ff33", "#ff99cc", "#66ff66"];
 let neonDarkColors = [
@@ -20,6 +28,12 @@ let neonDarkColors = [
 setInterval(() => changeColor(lbl_welcome, neonColors), 1000);
 setInterval(() => changeColor(lbl_info_head, neonDarkColors), 1500);
 
+setInterval(() => changeColor(lbl_Head_aboutme, neonColors), 1000);
+setInterval(() => changeColorClass(lbl_info_aboutme, neonDarkColors), 1500);
+
+setInterval(() => changeColor(lbl_Head_games, neonColors), 1000);
+setInterval(() => changeColor(lbl_info_games , neonDarkColors));
+
 function changeColor(element, colorArray) {
     if (!element || !colorArray || colorArray.length === 0) {
         console.error("Invalid input to changeElementColor");
@@ -28,6 +42,20 @@ function changeColor(element, colorArray) {
 
     let randomIndex = getRandomInt(0, colorArray.length - 1);
     element.style.color = colorArray[randomIndex];
+}
+
+function changeColorClass(elements , colorArray)
+{
+  if (!elements || !colorArray || colorArray.length === 0) {
+        console.error("Invalid input to changeColorClass");
+        return;
+    }
+
+    for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        let randomIndex = getRandomInt(0, colorArray.length - 1);
+        element.style.color = colorArray[randomIndex];
+    }
 }
 
 function getRandomInt(min, max) {
