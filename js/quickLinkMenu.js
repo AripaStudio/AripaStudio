22c061a3658cbd509f8ce650aecff98365d1674a
khashayar <aripastudio44@gmail.com>
2025-08-11 23:59:50 -0700

    add OtherLink for D Channel

--- js/quickLinkMenu.js
+++ js/quickLinkMenu.js
@@ -1,12 +1,23 @@
 
-        function toggleMenu() {
+        function toggleMenu() {            
             const menu = document.getElementById('quickLinksMenu');
             if (menu.classList.contains('show')) {
                 menu.classList.remove('show');
             } else {
                 menu.classList.add('show');
+            }           
+        }
+
+        function toggleMenuOtherLink() {
+             const menu2 = document.getElementById('quickOtherLink');
+            if(menu2.classList.contains('show')){
+                menu2.classList.remove('show');
+            }else{
+                menu2.classList.add('show')
             }
         }
+
+    
 
         // Ensure footer buttons are clickable
         document.querySelectorAll('.footer-button, .quick-links-button').forEach(button => {
